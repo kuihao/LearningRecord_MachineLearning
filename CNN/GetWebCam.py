aff517e1cd36fb0c1896134c24fc4fb1980736bd
Kuihao Chang <41053A041@gms.ndhu.edu.tw>
2020-12-28 15:08:35 +0800

    Learning ML with OpenCV

--- CNN/GetWebCam.py
+++ CNN/GetWebCam.py
@@ -16,7 +16,7 @@ Tips:
 # import cv2
 from cv2 import cv2 # In VScode use this code
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
@@ -24,3 +24,25 @@ else:
     print("hihi")
 
 print("fin")
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    # color to gray
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # 顯示圖片
+    cv2.imshow('frame', frame)
+    # 按下 q 鍵離開迴圈
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# 釋放該攝影機裝置
+cap.release()
+cv2.destroyAllWindows()
